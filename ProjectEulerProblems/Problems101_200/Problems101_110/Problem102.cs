7352ed01f85482d7f31dc105b482699231994aff
Dean Parker <dap2180@columbia.edu>
2018-02-11 13:23:02 -0500

    Problem 102 Completed, Simple Vector3 added

---
+++ ProjectEulerProblems/Problems101_200/Problems101_110/Problem102.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using ProjectEulerProblems.Structures;
+
+namespace ProjectEulerProblems
+{
+    public class Problem102
+    {
+        public static int Solve()
+        {
+            int count = 0;
+            string[] text = File.ReadAllLines(@"..\..\txt\Problem102Text.txt");
+            foreach(string line in text)
+            {
+                int[] points = Array.ConvertAll(line.Split(','), x => int.Parse(x));
+                Vector3[] triangle = new Vector3[3];
+                triangle[0] = new Vector3(points[0], points[1]);
+                triangle[1] = new Vector3(points[2], points[3]);
+                triangle[2] = new Vector3(points[4], points[5]);
+                if(InsideTriangle(triangle, Vector3.ZERO))
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        public static bool InsideTriangle(Vector3[] triangle, Vector3 point)
+        {
+            if(SameSide(triangle[0], triangle[1], triangle[2], point)
+                && SameSide(triangle[2], triangle[0], triangle[1], point)
+                && SameSide(triangle[1], triangle[2], triangle[0], point))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool SameSide(Vector3 a, Vector3 b, Vector3 p1, Vector3 p2)
+        {
+            Vector3 cross1 = (b - a).Cross(p1 - a);
+            Vector3 cross2 = (b - a).Cross(p2 - a);
+            if(cross1.Dot(cross2) >= 0)
+            {
+                return true;
+            }
+            return false;
+
+        }
+    }
+}
---
+++ ProjectEulerProblems/Structures/Vector3.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEulerProblems.Structures
+{
+    public class Vector3
+    {
+        public long X { get; set; }
+        public long Y { get; set; }
+        public long Z { get; set; }
+
+        public static readonly Vector3 ZERO = new Vector3(0, 0, 0);
+
+        public Vector3(long x, long y, long z)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
+        }
+
+        public Vector3(long x, long y) : this(x, y, 0) { }
+
+        public Vector3() : this(0, 0, 0) { }
+
+        public static Vector3 operator +(Vector3 a, Vector3 b)
+        {
+            return new Vector3(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+        }
+
+        public static Vector3 operator -(Vector3 a, Vector3 b)
+        {
+            return new Vector3(a.X - b.X, a.Y - b.Y, a.Z - b.Z);
+        }
+
+        public long Dot(Vector3 b)
+        {
+            return (this.X * b.X) + (this.Y * b.Y) + (this.Z * b.Z);
+        }
+
+        public Vector3 Cross(Vector3 b)
+        {
+            return new Vector3((this.Y * b.Z) - (b.Y * this.Z), -(this.X * b.Z) + (b.X * this.Z), (this.X * b.Y) - (b.X * this.Y));
+        }
+
+        public override string ToString()
+        {
+            return "(" + this.X + ", " + this.Y + ", " + this.Z + ")";
+        }
+    }
+}

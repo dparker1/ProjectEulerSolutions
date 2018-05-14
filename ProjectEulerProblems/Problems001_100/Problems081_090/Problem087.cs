513a316710fcab8fc3821f68e849943b3413f00c
Dean Parker <dap2180@columbia.edu>
2018-05-14 13:39:01 -0400

    Problem 87 Completed

---
+++ ProjectEulerProblems/Problems001_100/Problems081_090/Problem087.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEulerProblems
+{
+    public class Problem087
+    {
+        public static int Solve()
+        {
+            int goal = 50000000;
+            int limit = (int)Math.Ceiling(Math.Sqrt(goal));
+            List<int> primes = EulerUtilities.GeneratePrimes(limit).ConvertAll(x => (int)x);
+            HashSet<int> results = new HashSet<int>();
+
+            int soFar2, soFar3, soFar4;
+            for(int squared = 0; squared < primes.Count; squared++)
+            {
+                soFar2 = (int)Math.Pow(primes[squared], 2);
+                if(soFar2 > goal)
+                {
+                    break;
+                }
+                for(int cubed = 0; cubed < primes.Count; cubed++)
+                {
+                    soFar3 = soFar2 + (int)Math.Pow(primes[cubed], 3);
+                    if(soFar3 > goal)
+                    {
+                        break;
+                    }
+                    for(int fourth = 0; fourth < primes.Count; fourth++)
+                    {
+                        soFar4 = soFar3 + (int)Math.Pow(primes[fourth], 4);
+                        if(soFar4 > goal)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            results.Add(soFar4);
+                        }
+                    }
+                }
+            }
+
+            return results.Count;
+
+        }
+    }
+}
--- ProjectEulerProblems/Solution.cs
+++ ProjectEulerProblems/Solution.cs
@@ -12,7 +12,7 @@ namespace ProjectEulerProblems
         public static void Main(String[] args)
         {
             Stopwatch timer = Stopwatch.StartNew();
-            Console.WriteLine(Problem124.Solve());
+            Console.WriteLine(Problem087.Solve());
             timer.Stop();
             Console.WriteLine("Time: " + timer.ElapsedMilliseconds + " ms");
             Console.Read();

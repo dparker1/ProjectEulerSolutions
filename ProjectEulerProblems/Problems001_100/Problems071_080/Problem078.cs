993ebb4c00115b4b0126ae4f5253238c5bb8d7a8
Dean Parker <dap2180@columbia.edu>
2018-05-29 15:55:05 -0400

    Problem 78 Completed

---
+++ ProjectEulerProblems/Problems001_100/Problems071_080/Problem078.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEulerProblems
+{
+    public class Problem078
+    {
+        public static int Solve()
+        {
+            List<int> ways = new List<int>();
+            ways.Add(1);
+
+            for(int n = 1; n < int.MaxValue; n++)
+            {
+                ways.Add(0);
+                int i = 0;
+                int p = 1;
+                int sign = 0;
+
+                while(p <= n)
+                {
+                    if(sign > 1)
+                    {
+                        ways[n] -= ways[n - p];
+                    }
+                    else
+                    {
+                        ways[n] += ways[n - p];
+                    }
+                    ways[n] %= 1000000;
+                    sign = (sign + 1) % 4; 
+                    i++;
+
+                    int pn = (i / 2 + 1) * (i % 2 == 0 ? 1 : -1);
+                    p = pn * (3 * pn - 1) / 2; 
+                }
+                if(ways[n] == 0)
+                {
+                    return n;
+                }
+            }
+            return -1;
+        }
+    }
+}

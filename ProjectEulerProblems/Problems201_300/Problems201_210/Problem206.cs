da47a24a496ffd0f344ece90a563ee5daec163b3
Dean Parker <dap2180@columbia.edu>
2018-05-28 22:35:59 -0400

    Problem 206 Completed

---
+++ ProjectEulerProblems/Problems201_300/Problems201_210/Problem206.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace ProjectEulerProblems
+{
+    public class Problem206
+    {
+        public static string Solve()
+        {
+            char[] target = new char[] { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+            long n = 138902663;
+            long sq;
+            bool swit = true;
+            while(true)
+            {
+                sq = n * n;
+                if(Check(target, sq))
+                {
+                    break;
+                }
+                if(swit)
+                {
+                    n -= 6;
+                }
+                else
+                {
+                    n -= 4;
+                }
+                swit = !swit;
+            }
+            return (n * 10).ToString();
+        }
+
+        private static bool Check(char[] target, long n)
+        {
+            string ns = n.ToString();
+            for(int i = 0; i < ns.Length; i+=2)
+            {
+                if(ns[i] != target[i / 2])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
--- ProjectEulerProblems/Solution.cs
+++ ProjectEulerProblems/Solution.cs
@@ -12,7 +12,7 @@ namespace ProjectEulerProblems
         public static void Main(String[] args)
         {
             Stopwatch timer = Stopwatch.StartNew();
-            Console.WriteLine(Problem075.Solve());
+            Console.WriteLine(Problem206.Solve());
             timer.Stop();
             Console.WriteLine("Time: " + timer.ElapsedMilliseconds + " ms");
             Console.Read();

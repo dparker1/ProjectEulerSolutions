a1fc2b3e3aae2486595a84ec4fe5f6171b48e87a
Dean Parker <dap2180@columbia.edu>
2018-09-02 21:31:15 -0400

    Problem 347 Completed

---
+++ ProjectEulerProblems/Problems301_400/Problems341_350/Problem347.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEulerProblems
+{
+    public class Problem347
+    {
+        public static long Solve()
+        {
+            int limit = 10000000;
+            long sum = 0;
+            List<long> primes = EulerUtilities.GeneratePrimes(limit);
+            for(int p = 0; p < primes.Count; p++)
+            {
+                for(int q = p + 1; q < primes.Count; q++)
+                {
+                    long pMul = primes[p];
+                    long largest = 0;
+                    if(primes[p] * primes[q] > limit)
+                    {
+                        break;
+                    }
+                    while(pMul * primes[q] <= limit)
+                    {
+                        long inter = pMul * primes[q];
+                        while(inter <= limit)
+                        {
+                            if(inter > largest)
+                            {
+                                largest = inter;
+                            }
+                            inter *= primes[q];
+                        }
+                        pMul *= primes[p];
+                    }
+                    sum += largest;
+                }
+            }
+            return sum;
+        }
+    }
+}
--- ProjectEulerProblems/Solution.cs
+++ ProjectEulerProblems/Solution.cs
@@ -12,7 +12,7 @@ namespace ProjectEulerProblems
         public static void Main(String[] args)
         {
             Stopwatch timer = Stopwatch.StartNew();
-            Console.WriteLine(Problem346.Solve());
+            Console.WriteLine(Problem347.Solve());
             timer.Stop();
             Console.WriteLine("Time: " + timer.ElapsedMilliseconds + " ms");
             Console.Read();

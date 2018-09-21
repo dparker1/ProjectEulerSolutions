f0cdd4897370c2306c4f343136ed8a0329dee34c
Dean Parker <dap2180@columbia.edu>
2018-09-21 15:39:43 -0400

    Problem 323 Completed

---
+++ ProjectEulerProblems/Problems301_400/Problems321_330/Problem323.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEulerProblems
+{
+    public class Problem323
+    {
+        //https://math.stackexchange.com/questions/26167/expectation-of-the-maximum-of-i-i-d-geometric-random-variables
+        public static double Solve()
+        {
+            int n = 32;
+            double p = 0.5, sum = 0;
+            for(int k = 0; k <= 100; k++)
+            {
+                double f = 1 - Math.Pow(p, k);
+                sum += 1 - Math.Pow(f, n);
+            }
+            return sum;
+        }
+    }
+}
--- ProjectEulerProblems/Solution.cs
+++ ProjectEulerProblems/Solution.cs
@@ -12,7 +12,7 @@ namespace ProjectEulerProblems
         public static void Main(String[] args)
         {
             Stopwatch timer = Stopwatch.StartNew();
-            Console.WriteLine(Problem348.Solve());
+            Console.WriteLine(Problem323.Solve());
             timer.Stop();                                                                                                                                                                                              
             Console.WriteLine("Time: " + timer.ElapsedMilliseconds + " ms");
             Console.Read();

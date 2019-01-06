5fff66e4e6882089ace3e84f43b0fca2f68fcf9d
Dean Parker <dap2180@columbia.edu>
2019-01-05 20:07:37 -0500

    Problem 265 Completed

--- ProjectEulerProblems/Problems301_400/Problems341_350/Problem345.cs
+++ ProjectEulerProblems/Problems301_400/Problems341_350/Problem345.cs
@@ -46,11 +46,82 @@ namespace ProjectEulerProblems
                 }
             }
 
-            BipartiteGraph g = new BipartiteGraph();
-            g.ConstructFromMatrix(adjustedNumbers);
-            return g.MinCost();
+            return MinAssignmentCost(adjustedNumbers, numbers);
         }
 
-        
+        public static int MinAssignmentCost(int[][] a, int[][] original)
+        {
+            HashSet<int> takenRows = new HashSet<int>();
+            HashSet<int> takenCols = new HashSet<int>();
+            List<Tuple<int, int>> assignments = new List<Tuple<int, int>>();
+            while(takenRows.Count < a.Length)
+            {
+                Tuple<int, int> minIndex = MinimumFromRemaining(takenRows, takenCols, a);
+                assignments.Add(minIndex);
+                takenRows.Add(minIndex.Item1);
+                takenCols.Add(minIndex.Item2);
+            }
+            int[][] temp = EulerUtilities.DeepCopy(a);
+            foreach(var ass in assignments)
+            {
+                for(int c = 0; c < a[ass.Item1].Length; c++)
+                {
+                    temp[ass.Item1][c] -= a[ass.Item1][ass.Item2];
+                }
+            }
+            Tuple<int, int> leastReducedCost = Minimum(temp);
+            SwapAssignments(assignments, leastReducedCost.Item1, leastReducedCost.Item2);
+            return 0;
+        }
+
+        public static Tuple<int, int> MinimumFromRemaining(HashSet<int> takenR, HashSet<int> takenC, int[][] a)
+        {
+            int min = int.MaxValue;
+            Tuple<int, int> result = null;
+            for(int r = 0; r < a.Length; r++)
+            {
+                if(!takenR.Contains(r))
+                {
+                    for(int c = 0; c < a[r].Length; c++)
+                    {
+                        if(!takenC.Contains(c) && a[r][c] < min)
+                        {
+                            result = new Tuple<int, int>(r, c);
+                            min = a[r][c];
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+
+        public static Tuple<int, int> Minimum(int[][] a)
+        {
+            int min = int.MaxValue;
+            Tuple<int, int> result = null;
+            for(int r = 0; r < a.Length; r++)
+            {
+                for(int c = 0; c < a[r].Length; c++)
+                {
+                    if(a[r][c] < min)
+                    {
+                        min = a[r][c];
+                        result = new Tuple<int, int>(r, c);
+                    }
+                }
+            }
+            return result;
+        }
+
+        public static void SwapAssignments(List<Tuple<int, int>> assignments, int source, int sink)
+        {
+            var ass1 = assignments.Find(x => x.Item1 == source);
+            var ass2 = assignments.Find(x => x.Item2 == sink);
+            var temp1 = new Tuple<int, int>(ass1.Item1, ass2.Item2);
+            var temp2 = new Tuple<int, int>(ass2.Item1, ass1.Item2);
+            int i1 = assignments.IndexOf(ass1), i2 = assignments.IndexOf(ass2);
+            assignments[i1] = temp1;
+            assignments[i2] = temp2;
+        }
     }
 }
---
+++ ProjectEulerProblems/Problems301_400/Problems391_400/Problem392.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEulerProblems
+{
+    public class Problem392
+    {
+        public static double Solve()
+        {
+            double area = 100;
+            double minX = 0, minY = 0, minZ = 0, minQ = 0;
+            for(double x = 20; x <= 30; x += 0.1)
+            {
+                for(double y = 35; y <= 41; y += 0.1)
+                {
+                    for(double z = 50; z <= 60; z += 0.1)
+                    {
+                        for(double q = 60; q <= 70; q += 0.1)
+                        {
+                            double xRad = ToRad(x);
+                            double yRad = ToRad(y);
+                            double zRad = ToRad(z);
+                            double qRad = ToRad(q);
+                            double temp = Math.Sin(xRad) + Math.Cos(xRad) * (Math.Sin(yRad) - Math.Sin(xRad)) + Math.Cos(yRad) * (Math.Sin(zRad) - Math.Sin(yRad)) + Math.Cos(zRad) * (Math.Sin(qRad) - Math.Sin(zRad)) + Math.Cos(qRad) * (1 - Math.Sin(qRad));
+                            if(temp < area)
+                            {
+                                minX = x;
+                                minY = y;
+                                minZ = z;
+                                minQ = q;
+                                area = temp;
+                            }
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(minX);
+            Console.WriteLine(minY);
+            Console.WriteLine(minZ);
+            Console.WriteLine(minQ);
+            return area * 4;
+        }
+
+        private static double ToRad(double x)
+        {
+            return Math.PI * x / 180.0d;
+        }
+    }
+}

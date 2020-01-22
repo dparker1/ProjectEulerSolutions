12e52dec59bb047e5df30d86e7e2ae0a906bcf32
Dean Parker <dap2180@columbia.edu>
2020-01-22 17:17:17 -0500

    Problem 500 Completed

---
+++ ProjectEulerProblems/Problems401_500/Problems491_500/Problem500.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace ProjectEulerProblems
+{
+    public class Problem500
+    {
+        public static long Solve()
+        {
+            List<long> primes = EulerUtilities.GeneratePrimes(10000000);
+            SortedSet<long> sorted = new SortedSet<long>();
+            for(int i = 0; i < 500500; i++)
+            {
+                sorted.Add(primes[i]);
+            }
+            long result = 1;
+            long mod = 500500507;
+            int divisorPower = 0;
+            long x;
+            while(divisorPower < 500500)
+            {
+                x = sorted.Min;
+                result = (result * (x % mod)) % mod;
+                sorted.Remove(x);
+                sorted.Add(x * x);
+                divisorPower++;
+            }
+            return result;
+        }
+    }
+}
---
+++ ProjectEulerProblems/Problems601_700/Problems681_690/Problem684.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEulerProblems
+{
+    public class Problem684
+    {
+        public static int Solve()
+        {
+            ulong mod = 1000000007;
+            for(ulong i = 99999999; i < 1099999999; i += 100000000)
+            {
+                Console.WriteLine(i + ": " + i % mod);
+            }
+            Console.WriteLine();
+            for(ulong i = 999999999; i < 10999999999; i += 1000000000)
+            {
+                Console.WriteLine(i + ": " + i % mod);
+            }
+            Console.WriteLine();
+            for(ulong i = 9999999999; i < 109999999999; i += 10000000000)
+            {
+                Console.WriteLine(i + ": " + i % mod);
+            }
+            Console.WriteLine();
+            for(ulong i = 99999999999; i < 1099999999999; i += 100000000000)
+            {
+                Console.WriteLine(i + ": " + i % mod);
+            }
+            Console.WriteLine();
+            for(ulong i = 999999999999; i < 10999999999999; i += 1000000000000)
+            {
+                Console.WriteLine(i + ": " + i % mod);
+            }
+            Console.WriteLine();
+            for(ulong i = 9999999999999; i < 100999999999999; i+= 10000000000000)
+            {
+                Console.WriteLine(i + ": " + i % mod);
+            }
+            Console.WriteLine();
+            for(ulong i = 99999999999999; i < 1000999999999999; i += 100000000000000)
+            {
+                Console.WriteLine(i + ": " + i % mod);
+            }
+            Console.WriteLine();
+            for(ulong i = 999999999999999; i < 10000999999999999; i += 1000000000000000)
+            {
+                Console.WriteLine(i + ": " + i % mod);
+            }
+            Console.WriteLine();
+            for(ulong i = 9999999999999999; i < 100000999999999999; i += 10000000000000000)
+            {
+                Console.WriteLine(i + ": " + i % mod);
+            }
+            Console.WriteLine();
+            for(ulong i = 99999999999999999; i < 1000000999999999999; i += 100000000000000000)
+            {
+                Console.WriteLine(i + ": " + i % mod);
+            }
+            Console.WriteLine();
+            for(ulong i = 999999999999999999; i < 10000000999999999999; i += 1000000000000000000)
+            {
+                Console.WriteLine(i + ": " + i % mod);
+            }
+            Console.WriteLine();
+            for(ulong i = 9999999999999999999L; i < 10000000999999999999L; i += 10000000000000000000L)
+            {
+                Console.WriteLine(i + ": " + i % mod);
+            }
+            return 0;
+        }
+    }
+}

4fd6868420a1067f836eff5e4b116ede88349296
Dean Parker <dap2180@columbia.edu>
2020-04-06 14:32:10 -0400

    Problem 90 Completed

---
+++ ProjectEulerProblems/Problems001_100/Problems081_090/Problem090.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEulerProblems
+{
+    public class Problem090
+    {
+        public static int Solve()
+        {
+            List<int> nums = new List<int>() {0, 1, 2, 3, 4, 5, 6, 7, 8, 6 };
+            List<IEnumerable<int>> combos = nums.Combinations(6).ToList();
+            List<Tuple<int, int>> goals = new List<Tuple<int, int>>(){new Tuple<int, int>(0, 1),
+                                                                      new Tuple<int, int>(0, 4),
+                                                                      new Tuple<int, int>(0, 6),
+                                                                      new Tuple<int, int>(1, 6),
+                                                                      new Tuple<int, int>(2, 5),
+                                                                      new Tuple<int, int>(3, 6),
+                                                                      new Tuple<int, int>(4, 6),
+                                                                      new Tuple<int, int>(6, 4),
+                                                                      new Tuple<int, int>(8, 1)};
+            int count = 0;
+            for(int i = 0; i < combos.Count - 1; i++)
+            {
+                for(int j = i + 1; j < combos.Count; j++)
+                {
+                    bool canMake = true;
+                    foreach(Tuple<int, int> goal in goals)
+                    {
+                        if((!combos[i].Contains(goal.Item1) || !combos[j].Contains(goal.Item2)) && (!combos[j].Contains(goal.Item1) || !combos[i].Contains(goal.Item2)))
+                        {
+                            canMake = false;
+                            break;
+                        }
+                    }
+                    if(canMake)
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}

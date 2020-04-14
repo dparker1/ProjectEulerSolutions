e00f49afd3477ec9ba27729c317b57218424f2f4
Dean Parker <dap2180@columbia.edu>
2020-04-13 22:28:08 -0400

    Problem 700 Completed

---
+++ ProjectEulerProblems/Problems601_700/Problems691_700/Problem700.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEulerProblems
+{
+    public class Problem700
+    {
+        public static string Solve()
+        {
+            long diff = 1504170715041707;
+            long mod = 4503599627370517;
+            long sum = 0;
+            long high = diff;
+            long low = diff;
+            long next;
+            while(low > 0)
+            {
+                next = (high + low) % mod;
+                if(next < low)
+                {
+                    sum += low;
+                    low = next;
+                }
+                else
+                {
+                    high = next;
+                }
+            }
+            return sum.ToString();
+        }
+    }
+}
--- ProjectEulerProblems/Solution.cs
+++ ProjectEulerProblems/Solution.cs
@@ -12,7 +12,7 @@ namespace ProjectEulerProblems
         public static void Main(String[] args)
         {
             Stopwatch timer = Stopwatch.StartNew();
-            Console.WriteLine(Problem351.Solve());
+            Console.WriteLine(Problem700.Solve());
             timer.Stop();
             Console.WriteLine("Time: " + timer.ElapsedMilliseconds + " ms");
             Console.Read();

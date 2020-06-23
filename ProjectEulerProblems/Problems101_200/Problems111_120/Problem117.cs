9c12ab6d5586b7fa6361f301cc4b3e1e48ed574e
Dean Parker <dap2180@columbia.edu>
2020-06-23 10:40:11 -0400

    Problem 117 Completed

---
+++ ProjectEulerProblems/Problems101_200/Problems111_120/Problem117.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEulerProblems
+{
+    public class Problem117
+    {
+        public static long Solve()
+        {
+            int length = 50;
+            long[] arr = new long[length + 1];
+            return Count(arr, 2, 4, length);
+        }
+
+        public static long Count(long[] arr, int minBlockSize, int maxBlockSize, int lengthLeft)
+        {
+            if(lengthLeft < minBlockSize)
+            {
+                return 1;
+            }
+            if(arr[lengthLeft] > 0)
+            {
+                return arr[lengthLeft];
+            }
+
+            long total = 1;
+            for(int blockSize = minBlockSize; blockSize <= maxBlockSize; blockSize++)
+            {
+                for(int j = 0; j <= lengthLeft - blockSize; j++)
+                {
+                    total += Count(arr, minBlockSize, maxBlockSize, lengthLeft - blockSize - j);
+                }
+            }
+            arr[lengthLeft] = total;
+            return total;
+        }
+    }
+}
--- ProjectEulerProblems/Solution.cs
+++ ProjectEulerProblems/Solution.cs
@@ -12,7 +12,7 @@ namespace ProjectEulerProblems
         public static void Main(String[] args)
         {
             Stopwatch timer = Stopwatch.StartNew();
-            Console.WriteLine(Problem116.Solve());
+            Console.WriteLine(Problem117.Solve());
             timer.Stop();
             Console.WriteLine("Time: " + timer.ElapsedMilliseconds + " ms");
             Console.Read();

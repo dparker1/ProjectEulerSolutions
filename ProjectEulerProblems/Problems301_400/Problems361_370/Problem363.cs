be58364fff8dd7874fb22bd3d2f5beec383ea648
Dean Parker <dap2180@columbia.edu>
2020-07-12 15:36:07 -0400

    Problem 363 Completed

---
+++ ProjectEulerProblems/Problems301_400/Problems361_370/Problem363.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEulerProblems
+{
+    public class Problem363
+    {
+        /*
+         * x = (1-t)^3 + 3*t*(1-t)^2 + 3*t^2*(1-t)*v
+         * y = t^3 + 3*t*v*(1-t)^2 + 3*t^2*(1-t)
+         * dx/dt = -9*t^2*v + 6*t^2 + 6*t*v - 6*t
+         * dy/dt = 9*t^2*v - 6*t^2 - 12*t*v + 6*t + 3*v
+         * 
+         * integral(y * dx/dt)dt from 1 to 0 = (-3*v^2 + 12v + 10) / 20
+         * 
+         * integral(sqrt((dx/dt)^2 + (dy/dt)^2))dt) from 0 to 1 = 1.570796911273925168510196517
+         */
+        public static double Solve()
+        {
+            double goal = Math.PI / 2;
+            
+            return 100 * (1.570796911273925168510196517 - goal) / goal;
+        }
+    }
+}
--- ProjectEulerProblems/Solution.cs
+++ ProjectEulerProblems/Solution.cs
@@ -12,7 +12,7 @@ namespace ProjectEulerProblems
         public static void Main(String[] args)
         {
             Stopwatch timer = Stopwatch.StartNew();
-            Console.WriteLine(Problem117.Solve());
+            Console.WriteLine(Problem363.Solve());
             timer.Stop();
             Console.WriteLine("Time: " + timer.ElapsedMilliseconds + " ms");
             Console.Read();

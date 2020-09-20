78fb4cc3eebe6de34843af15b968f0667bd19ad7
Dean Parker <dap2180@columbia.edu>
2020-09-20 17:23:31 -0400

    Problem 689 Completed

---
+++ ProjectEulerProblems/Problems601_700/Problems681_690/Problem689.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEulerProblems
+{
+    public class Problem689
+    {
+        static double ToRad = Math.PI / 180;
+        static double TwoPi = Math.PI * 2;
+        static double PiSq = Math.PI * Math.PI;
+        public static double Solve()
+        {
+            double result = 0;
+            for(int i = 0; i < 200; i++)
+            {
+                int t = 2 * i + 1;
+                result += Math.Sin(TwoPi * t * PiSq / 12) * g(TwoPi * t) / t;
+            }
+            result /= Math.PI;
+            result += 0.25;
+            return 1 - result;
+        }
+
+        public static double g(double t)
+        {
+            double res = 1;
+            for(int i = 1; i < 500; i++)
+            {
+                res *= Math.Cos(t / (2 * i * i));
+            }
+            return res;
+        }
+    }
+}

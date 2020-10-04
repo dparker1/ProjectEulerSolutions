be09b2981af422b85aaa8410036eaff039e513ff
Dean Parker <dap2180@columbia.edu>
2020-10-04 19:15:34 -0400

    Problem 231 Completed

---
+++ ProjectEulerProblems/Problems201_300/Problems231_240/Problem231.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEulerProblems
+{
+    public class Problem231
+    {
+        public static long Solve()
+        {
+            int n = 20000000;
+            int r = 15000000;
+            int n_r = n - r;
+            List<long> primes = EulerUtilities.GeneratePrimes(20000000);
+            long sum = 0;
+            for (int p = 0; p < primes.Count; p++)
+            {
+                long prime = primes[p];
+                while(prime < n)
+                {
+                    sum += primes[p] * (n / prime - r / prime - n_r / prime);
+                    prime *= primes[p];
+                }
+            }
+            return sum;
+        }
+    }
+}

fa39ec1096b60c98e271cc7ebfd62ced4879152f
Dean Parker <dap2180@columbia.edu>
2020-10-04 23:01:37 -0400

    Problem 301 Completed

---
+++ ProjectEulerProblems/Problems301_400/Problems301_310/Problem301.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEulerProblems
+{
+    public class Problem301
+    {
+        public static int Solve()
+        {
+            int count = 0;
+            for(int n = 1; n <= 1 << 30; n++)
+            {
+                if((n ^ (2*n) ^ (3*n)) == 0)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}

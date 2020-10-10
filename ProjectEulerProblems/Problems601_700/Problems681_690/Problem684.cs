e0ebb1b9b93becc7bdab561833b421e065917d4d
Dean Parker <dap2180@columbia.edu>
2020-10-09 22:33:30 -0400

    Problem 684 Completed

--- ProjectEulerProblems/Problems601_700/Problems681_690/Problem684.cs
+++ ProjectEulerProblems/Problems601_700/Problems681_690/Problem684.cs
@@ -10,38 +10,24 @@ namespace ProjectEulerProblems
     public class Problem684
     {
         static BigInteger mod = 1000000007;
-        static BigInteger modLessDiv = ((mod - 1) / 2);
-        static BigInteger[] speed = new BigInteger[19];
         public static string Solve()
         {
             BigInteger sum = 0;
-            speed[0] = 1;
-            for(int i = 1; i < 19; i++)
-            {
-                speed[i] = BigInteger.ModPow(10, i, mod);
-            }
-            List<long> fibonacci = EulerUtilities.FibonacciLimit(91);
+            List<long> fibonacci = EulerUtilities.FibonacciLimit(90);
             fibonacci.RemoveAt(0);
             for(int i = 0; i < fibonacci.Count; i++)
             {
-                sum += S(fibonacci[i]);
+                sum = sum + S(fibonacci[i]);
                 sum = sum % mod;
-
             }
             return sum.ToString();
         }
 
         public static BigInteger S(BigInteger n)
         {
-            BigInteger b = n / 9;
-            BigInteger a = n % 9;
-            int r = (int)(b % 18);
-            BigInteger c = (b / 18) % (modLessDiv);
-            BigInteger d = BigInteger.ModPow(49, c, mod);
-            Console.WriteLine(d);
-            d = (d * speed[r]) % mod;
-            BigInteger S = d * (a + 6 + (a * (1 + a) / 2)) - (a + 9 * b + 6);
-            return S % mod;
+            BigInteger a = n / 9;
+            BigInteger b = n % 9;
+            return 6*BigInteger.ModPow(10, a, mod) - (9 * a) % mod - 6 + b*(b+3)/2*BigInteger.ModPow(10, a, mod) - b;
         }
     }
 }
